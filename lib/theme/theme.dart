26ec1e65ef89a24f25117c1be784f6b32ce268b4
abdul <rehmanshaikh0796@gmail.com>
2025-10-23 23:48:22 +0530

    theme changes done

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import '../color/colors.dart';
+import '../utils/colors.dart';
 
 class AppTheme {
   AppTheme._();
@@ -13,14 +13,19 @@ class AppTheme {
       primaryColor: AppColors.whiteColor,
       scaffoldBackgroundColor: AppColors.whiteColor,
       splashFactory: NoSplash.splashFactory,
+      focusColor: AppColors.transparent,
+      hoverColor: AppColors.transparent,
       pageTransitionsTheme: PageTransitionsTheme(
         builders: {
           TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
           TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
         },
       ),
-      appBarTheme: AppBarTheme(color: AppColors.whiteColor, elevation: 25.0),
-      iconTheme: const IconThemeData(color: Colors.black),
+      appBarTheme: AppBarTheme(
+        backgroundColor: AppColors.whiteColor,
+        elevation: 25.0,
+      ),
+      iconTheme: const IconThemeData(color: AppColors.blackColor),
     );
   }
 
@@ -30,7 +35,7 @@ class AppTheme {
       hoverColor: AppColors.transparent,
       highlightColor: AppColors.transparent,
       splashColor: AppColors.transparent,
-      primaryColor: AppColors.primaryColor,
+      primaryColor: AppColors.blackColor,
       splashFactory: NoSplash.splashFactory,
       appBarTheme: AppBarTheme(
         backgroundColor: AppColors.darkBackground,
