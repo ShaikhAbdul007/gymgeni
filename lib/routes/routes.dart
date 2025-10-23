6c825e25903fb388ba52eea2d619e7ab15a24bd9
abdul <rehmanshaikh0796@gmail.com>
2025-10-23 23:48:13 +0530

    new route added

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,28 +1,50 @@
 import 'package:get/get.dart';
 import 'package:gymgeni/module/drawer/view/drawer_view.dart';
 import 'package:gymgeni/module/dashboard/binding/dashboard_binding.dart';
+import 'package:gymgeni/module/employee/view/employee_view.dart';
+import 'package:gymgeni/module/lead/view/lead_view.dart';
+import 'package:gymgeni/module/login/views/loginscreen_view.dart';
+import 'package:gymgeni/module/member/binding/member_bindings.dart';
+import 'package:gymgeni/module/reports/view/report_view.dart';
 
 import '../module/drawer/binding/drawer_binding.dart';
 import '../module/dashboard/view/dashboard_view.dart';
+import '../module/login/bindings/loginscreen_binding.dart';
 import '../module/member/view/members_view.dart';
-import '../module/payment/view/payment_view.dart';
+import '../module/finance/view/finance_view.dart';
 import 'routes_path.dart';
 
 class AppRoutes {
   AppRoutes._();
 
-  static const initial = RoutesPaths.mainScreen;
+  static const initial = RoutesPaths.loginView;
 
   static final routes = [
     GetPage(
-      name: RoutesPaths.mainScreen,
+      name: RoutesPaths.dashboardView,
       participatesInRootNavigator: true,
       page: () => DashboardView(),
       bindings: [DashboardBinding(), DrawerBinding()],
     ),
 
-    GetPage(name: RoutesPaths.memberScreen, page: () => const MembersView()),
-    GetPage(name: RoutesPaths.paymentScreen, page: () => const PaymentView()),
-    GetPage(name: RoutesPaths.customDrawer, page: () => const CustomDrawer()),
+    GetPage(
+      name: RoutesPaths.memberView,
+      page: () => const MembersView(),
+      binding: MemberBindings(),
+    ),
+    GetPage(name: RoutesPaths.financeView, page: () => const FinanceView()),
+    GetPage(
+      name: RoutesPaths.customDrawerView,
+      page: () => const CustomDrawer(),
+      binding: DrawerBinding(),
+    ),
+    GetPage(name: RoutesPaths.leadView, page: () => const LeadView()),
+    GetPage(name: RoutesPaths.reportView, page: () => const ReportView()),
+    GetPage(name: RoutesPaths.employeeView, page: () => const EmployeeView()),
+    GetPage(
+      name: RoutesPaths.loginView,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
   ];
 }
--- lib/routes/routes_path.dart
+++ lib/routes/routes_path.dart
@@ -2,103 +2,43 @@ import 'package:get/get.dart';
 
 abstract class RoutesPaths {
   RoutesPaths._();
-  static const home = '/home';
-  static const onBoard = '/onboard';
-  static const customDrawer = '/customdrawer';
-  static const splashScreen = '/splashscreen';
-  static const loginScreen = '/loginscreen';
-  static const registrationScreen = '/registrationscreen';
-  static const forgotPasswordScreen = '/forgotpasswordscreen';
-  static const otpScreen = '/otp-screen';
-  static const dashBoardScreen = '/dashBoardScreen';
-  static const workoutScreen = '/workoutScreen';
-  static const reportScreen = '/reportScreen';
-  static const memberScreen = '/memberScreen';
-  static const paymentScreen = '/paymentScreen';
-  static const mainScreen = '/mainScreen';
-  static const manageplanview = '/manageplanview';
-  static const signupScreen = '/signupScreen';
-  static const attendenceScreen = '/attendenceScreen';
-  static const staffAttendenceScreen = '/staffAttendenceScreen';
-  static const allStaffScreen = '/allStaffScreen';
-  static const inActiveMemberScreen = '/inActiveMemberScreen';
-  static const activeMemberScreen = '/activeMemberScreen';
-  static const allLeadScreen = '/allLeadScreen';
-  static const leadFollowHistoryScreen = '/leadFollowHistoryScreen';
-  static const leadMasterScreen = '/leadMasterScreen';
-  static const freezeMemberScreen = '/freezeMemberScreen';
-  static const paymentMasterScreen = '/paymentMasterScreen';
-  static const saleScreen = '/saleScreen';
-  static const memberMaster = '/memberMaster';
-  static const unknownRoute = '/unknownRoute';
+
+  static const dashboardView = '/dashboardView';
+  static const memberView = '/memberView';
+  static const financeView = '/financeView';
+  static const customDrawerView = '/customDrawerView';
+  static const leadView = '/leadView';
+  static const loginView = '/loginView';
+  static const signUpView = '/signUpView';
+  static const reportView = '/reportView';
+  static const unknownRouteView = '/unknownRouteView';
+  static const employeeView = '/employeeView';
 
   static navigateToRoute({required String routeName, dynamic data}) {
     switch (routeName) {
-      case loginScreen:
-        Get.offAllNamed(loginScreen);
+      case loginView:
+        Get.offAllNamed(loginView);
         break;
-      case otpScreen:
-        Get.toNamed(otpScreen);
+      case signUpView:
+        Get.toNamed(signUpView);
         break;
-      case registrationScreen:
-        Get.toNamed(registrationScreen);
+      case memberView:
+        Get.toNamed(memberView);
         break;
-      case forgotPasswordScreen:
-        Get.toNamed(forgotPasswordScreen);
+      case reportView:
+        Get.toNamed(reportView);
         break;
-      case dashBoardScreen:
-        Get.toNamed(dashBoardScreen);
+      case financeView:
+        Get.toNamed(financeView);
         break;
-      case memberScreen:
-        Get.toNamed(memberScreen);
+      case dashboardView:
+        Get.offAllNamed(dashboardView);
         break;
-      case reportScreen:
-        Get.toNamed(reportScreen);
-        break;
-      case workoutScreen:
-        Get.toNamed(workoutScreen);
-        break;
-      case paymentScreen:
-        Get.toNamed(paymentScreen);
-        break;
-      case mainScreen:
-        Get.offAllNamed(mainScreen);
-        break;
-      case manageplanview:
-        Get.toNamed(manageplanview);
-        break;
-      case signupScreen:
-        Get.toNamed(signupScreen);
-        break;
-      case memberMaster:
-        Get.rootDelegate.toNamed(memberMaster);
-        break;
-      case attendenceScreen:
-        Get.toNamed(attendenceScreen);
-        break;
-      case staffAttendenceScreen:
-        Get.toNamed(staffAttendenceScreen);
-        break;
-      // case staffManageScreen:
-      //   Get.toNamed(staffManageScreen);
-      //   break;
-      case leadFollowHistoryScreen:
-        Get.toNamed(leadFollowHistoryScreen);
-        break;
-      case leadMasterScreen:
-        Get.toNamed(leadMasterScreen);
-        break;
-      case freezeMemberScreen:
-        Get.toNamed(freezeMemberScreen);
-        break;
-      case paymentMasterScreen:
-        Get.toNamed(paymentMasterScreen);
-        break;
-      case saleScreen:
-        Get.toNamed(saleScreen);
+      case leadView:
+        Get.toNamed(leadView);
         break;
       default:
-        Get.offNamed(unknownRoute);
+        Get.offNamed(unknownRouteView);
     }
   }
 }
