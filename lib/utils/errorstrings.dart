3c016585c842605cf53fce5b0c7991e277c6200b
abdul <rehmanshaikh0796@gmail.com>
2025-10-23 23:48:28 +0530

    error added

---
+++ lib/utils/errorstrings.dart
@@ -0,0 +1,3 @@
+const String somethingWentWrong = 'Request Failed,something went wrong';
+const String internetError = 'No Internate, please check';
+const String httpError = "Can't connect to server";
