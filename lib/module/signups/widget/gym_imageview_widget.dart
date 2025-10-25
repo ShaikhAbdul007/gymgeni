5eefde1a0823af9eb24247a11197a04e0a442b07
abdul <rehmanshaikh0796@gmail.com>
2025-10-25 18:39:46 +0530

    GymImageViewWidget added

---
+++ lib/module/signups/widget/gym_imageview_widget.dart
@@ -0,0 +1,38 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import '../../../helper/button.dart';
+import '../../../utils/sizebox.dart';
+
+class GymImageViewWidget extends StatelessWidget {
+  final Uint8List image;
+  final void Function() okOnPress;
+  final void Function() clearOnPress;
+  const GymImageViewWidget({
+    super.key,
+    required this.image,
+    required this.okOnPress,
+    required this.clearOnPress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.memory(image, width: 300, height: 400),
+        CustomButton(
+          height: 30,
+          width: 200,
+          label: 'Clear Image',
+          onPress: clearOnPress,
+        ),
+        CustomButtonWithOutBackGroudColor(
+          height: 30,
+          width: 100,
+          label: 'Ok',
+          onPress: okOnPress,
+        ),
+      ],
+    );
+  }
+}
